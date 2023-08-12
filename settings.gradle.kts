23982bbfd3db63fe6517f75a1e80cc4a983f9edc
elkhoudiry <ahmedelkhodery2@Gmail.com>
2023-08-12 22:29:41 +0300

    remove sql module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,11 +23,5 @@ rootProject.name = "klogger"
 
 include(":server")
 
-include(":core-caching-sqldelight")
-project(":core-caching-sqldelight").projectDir = file("core/caching/sqldelight")
-
-include(":core-caching-sqldelight-test")
-project(":core-caching-sqldelight-test").projectDir = file("core/caching/sqldelight-test")
-
 include(":core-shared")
 project(":core-shared").projectDir = file("core/shared")
