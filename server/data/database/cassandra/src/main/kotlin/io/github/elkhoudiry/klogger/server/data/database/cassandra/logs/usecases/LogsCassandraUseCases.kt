b18fdb0692f06a713a469e0a985cdf667e5fe23f
elkhoudiry <ahmedelkhodery2@Gmail.com>
2024-02-10 12:46:14 +0200

    migration

--- server/data/database/cassandra/src/main/kotlin/io/github/elkhoudiry/klogger/server/data/database/cassandra/logs/usecases/LogsCassandraUseCases.kt
+++ server/data/database/cassandra/src/main/kotlin/io/github/elkhoudiry/klogger/server/data/database/cassandra/logs/usecases/LogsCassandraUseCases.kt
@@ -6,9 +6,9 @@ internal class LogsCassandraUseCases(
     session: CqlSession,
     keyspace: String
 ) {
-    val getAll = GetAllUseCase(session = session, keyspace = keyspace, table = "logs")
+    val getAll = GetAllUseCase(session = session, keyspace = keyspace, table = "log")
 
-    val getById = GetByIdUseCase(session = session, keyspace = keyspace, table = "logs")
+    val getById = GetByIdUseCase(session = session, keyspace = keyspace, table = "log")
 
-    val insert = InsertUseCase(session, keyspace = keyspace, table = "logs")
+    val insert = InsertUseCase(session, keyspace = keyspace, table = "log")
 }
